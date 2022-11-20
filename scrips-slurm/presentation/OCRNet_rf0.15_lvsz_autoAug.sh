6cc0e236ad03e090f56cfd925783e0f82f883382
Blizzard416 <wentao.zhou@mail.utoronto.ca>
2022-11-20 17:37:46 -0500

    Update compute canada script

--- scrips-slurm/presentation/OCRNet_rf0.15_lvsz_autoAug.sh
+++ scrips-slurm/presentation/OCRNet_rf0.15_lvsz_autoAug.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=63500M
 #SBATCH --time=20:00:00
-#SBATCH --output=/home/njrttx/scratch/CSC490_OpenYourEyes/1030_exp/Baseline-DLV3.out
+#SBATCH --output=/home/njrttx/scratch/CSC490_OpenYourEyes/presentation_output/OCRNet_rf0.15_lvsz_autoAug.out
 
 echo "Current working directory: pwd"
 
@@ -12,4 +12,4 @@ cd $SCRATCH
 ./setup.sh
 source ./490_env/bin/activate
 cd /home/njrttx/scratch/CSC490_OpenYourEyes
-python main.py -c configs/DeepLabV3Plus_baseline.json --task 2 -d 0 --data_path "/home/njrttx/scratch/CSC490_OpenYourEyes/data"
+python main.py -c configs/presentation/OCRNet_rf0.15_lvsz_autoAug.json --task 2 -d 0 --data_path "/home/njrttx/scratch/CSC490_OpenYourEyes/data"
---
+++ scrips-slurm/presentation/SegFormer_rf0.15_lvsz_trivialAug.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=63500M
+#SBATCH --time=20:00:00
+#SBATCH --output=/home/njrttx/scratch/CSC490_OpenYourEyes/presentation_output/SegFormer_rf0.15_lvsz_trivialAug.out
+
+echo "Current working directory: pwd"
+
+cd $SCRATCH
+./setup.sh
+source ./490_env/bin/activate
+cd /home/njrttx/scratch/CSC490_OpenYourEyes
+python main.py -c configs/presentation/SegFormer_rf0.15_lvsz_trivialAug.json --task 2 -d 0 --data_path "/home/njrttx/scratch/CSC490_OpenYourEyes/data"
